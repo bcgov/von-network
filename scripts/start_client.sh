adcf9a0dcd7da7b5962f9eafb83eaf42d5e1deae
Nicholas Rempel <nbrempel@gmail.com>
2017-11-28 14:05:23 -0800

    update start scripts for cli and webserver to be aware of node procies

--- scripts/start_client.sh
+++ scripts/start_client.sh
@@ -2,10 +2,16 @@
 
 set -e
 
-if [ -z "$IP" ]; then
-    von_generate_transactions
-else
+if [ ! -z "$IPS" ]; then
+    echo von_generate_transactions -s "$IPS"
+    von_generate_transactions -s "$IPS"
+elif [ ! -z "$IP" ]; then
+    echo von_generate_transactions -i "$IP"
     von_generate_transactions -i "$IP"
+else
+    echo von_generate_transactions
+    von_generate_transactions
 fi
+
 
 indy
