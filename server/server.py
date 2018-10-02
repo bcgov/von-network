66d92bb93ed280f3940e49a830895da31c2a13b2
Andrew Whitehead <andrew@1crm.com>
2018-10-02 11:23:22 -0700

    fetch ledger transactions using GET_TXN instead of reading ledger data directly; cache in sqlite database

--- server/server.py
+++ server/server.py
@@ -1,57 +1,33 @@
 from datetime import datetime
 import json
+import logging
 import os
 import shutil
-import subprocess
-import re
+import yaml
 
 from aiohttp import web
 
-from .anchor import AnchorHandle, NotReadyException
+from .anchor import (
+  AnchorHandle,
+  NotReadyException,
+  INDY_ROLE_TYPES,
+  INDY_TXN_TYPES,
+)
+
+logging.basicConfig(level=os.getenv('LOG_LEVEL', logging.INFO))
+LOGGER = logging.getLogger(__name__)
 
 
 PATHS = {
   'python': shutil.which('python3'),
-  'validator-info': shutil.which('validator-info'),
-  'read_ledger': shutil.which('read_ledger'),
 }
-
-INDY_TXN_TYPES = {
-  "0": "NODE",
-  "1": "NYM",
-  "3": "GET_TXN",
-  "100": "ATTRIB",
-  "101": "SCHEMA",
-  "102": "CLAIM_DEF",
-  "103": "DISCO",
-  "104": "GET_ATTR",
-  "105": "GET_NYM",
-  "107": "GET_SCHEMA",
-  "108": "GET_CLAIM_DEF",
-  "109": "POOL_UPGRADE",
-  "110": "NODE_UPGRADE",
-  "111": "POOL_CONFIG",
-  "112": "CHANGE_KEY",
-}
-
-INDY_ROLE_TYPES = {
-  "0": "TRUSTEE",
-  "2": "STEWARD",
-  "100": "TGB",
-  "101": "TRUST_ANCHOR",
-}
-
-INDY_LEDGER_TYPES = {
-  "0": "DOMAIN",
-  "1": "POOL",
-  "2": "CONFIG",
-}
-
 
 os.chdir(os.path.dirname(__file__))
 
 APP = web.Application()
 ROUTES = web.RouteTableDef()
+TRUST_ANCHOR = AnchorHandle()
+
 
 @ROUTES.get('/')
 async def index(request):
@@ -63,58 +39,18 @@ async def favicon(request):
 
 ROUTES.static('/include', './static/include')
 
-TRUST_ANCHOR = AnchorHandle()
 
-
-def json_response(data):
+def json_response(data, status=200, **kwargs):
   # FIXME - use aiohttp-cors
-  headers = {'Access-Control-Allow-Origin': '*'}
-  return web.json_response(data, headers=headers)
+  kwargs['headers'] = {'Access-Control-Allow-Origin': '*'}
+  kwargs['text'] = json.dumps(data, indent=2, sort_keys=True)
+  if 'content_type' not in kwargs:
+    kwargs['content_type'] = 'application/json'
+  return web.Response(status=status, **kwargs)
 
 
-def validator_info(node_name, as_json=True):
-  args = [PATHS['validator-info']]
-  if as_json:
-    args.append("--json")
-  else:
-    args.append("-v")
-  args.extend(["--basedir", "/home/indy/.mnt/" + node_name + "/sandbox/"])
-  proc = subprocess.run(args, stdout=subprocess.PIPE, universal_newlines=True)
-  if as_json:
-    # The result is polluted with logs in the latest version.
-    # We pull out json
-    m = re.search(r'(?s)\n({.*})', proc.stdout)
-    corrected_stdout = m.group(1) if m else proc.stdout
-    return json.loads(corrected_stdout)
-  return proc
-
-
-def read_ledger(ledger, seq_no=0, seq_to=1000, node_name='node1', format="data"):
-  if ledger != "domain" and ledger != "pool" and ledger != "config":
-    raise ValueError("Unsupported ledger type: {}".format(ledger))
-  args = [PATHS['read_ledger'], "--type", ledger]
-  if seq_no > 0:
-    args.extend(["--seq_no", str(seq_no)])
-  args.extend(["--to", str(seq_to)])
-  #args.extend(["--base_dir", "/home/indy/.mnt/" + node_name])
-  args.extend(["--node_name", node_name])
-  proc = subprocess.run(args, stdout=subprocess.PIPE, universal_newlines=True)
-
-  if format == "pretty" or format == "data":
-    lines = proc.stdout.splitlines()
-    resp = []
-    for line in lines:
-      _seq_no, txn = line.split(' ', 2)
-      parsed = json.loads(txn)
-      if format == "pretty":
-        parsed = json.dumps(parsed, indent=4, sort_keys=True)
-      resp.append(parsed)
-    if format == "pretty":
-      return "\n\n".join(resp)
-    return resp
-
-  # format = json
-  return proc.stdout
+def not_ready():
+  return web.json_response(data={"detail": "Not ready"}, status=503)
 
 
 @ROUTES.get("/status")
@@ -122,101 +58,145 @@ async def status(request):
   try:
     response = await TRUST_ANCHOR.validator_info()
   except NotReadyException:
-    return web.Response(status=503)
+    return not_ready()
   return json_response(response)
 
 
 @ROUTES.get("/status/text")
 async def status_text(request):
-  nodes = ["node1", "node2", "node3", "node4"]
+  try:
+    response = await TRUST_ANCHOR.validator_info()
+  except NotReadyException:
+    return not_ready()
 
-  response_text = ""
-  for idx,node_name in enumerate(nodes):
-    proc = validator_info(node_name, as_json=False)
-    if idx > 0:
-      response_text += "\n"
-    response_text += node_name + "\n\n" + proc.stdout
+  text = []
+  for node in response:
+    id = node["Node_info"]["Name"]
+    text.append(id)
+    text.append("")
+    text.append(yaml.dump(node))
+    text.append("")
 
-  return web.Response(text=response_text)
-
-
-@ROUTES.get("/status/pretty")
-async def status_pretty(request):
-  response = await validator_info_request()
-  data = json.dumps(parsed, indent=4, sort_keys=True)
-  return web.Response(text=data)
+  return web.Response(text="\n".join(text))
 
 
 @ROUTES.get("/ledger/{ledger_name}")
 async def ledger_json(request):
-  response = read_ledger(request.match_info['ledger_name'], format="json")
-  return web.Response(text=response)
+  #response = read_ledger(request.match_info['ledger_name'], format="json")
+  #return web.Response(text=response)
 
+  if not TRUST_ANCHOR.ready:
+    return not_ready()
 
-@ROUTES.get("/ledger/{ledger_name}/pretty")
-async def ledger_pretty(request):
-  response = read_ledger(request.match_info['ledger_name'], format="pretty")
-  return web.Response(text=response)
+  page = int(request.query.get('page', 1))
+  page_size = int(request.query.get('page_size', 100))
+  start = (page - 1) * page_size + 1
+  end = start + page_size - 1
+
+  rows = await TRUST_ANCHOR.get_txn_range(request.match_info["ledger_name"], start, end)
+  last_modified = None
+  results = []
+  for row in rows:
+    last_modified = max(last_modified, row[1]) if last_modified else row[1]
+    results.append(json.loads(row[2]))
+  latest = await TRUST_ANCHOR.get_latest_seqno(request.match_info["ledger_name"])
+  if not results:
+    data = {
+      "detail": "Invalid page."
+    }
+    response = json_response(data, status=404)
+  else:
+    data = {
+      "total": latest,
+      "page_size": page_size,
+      "page": page,
+      "first_index": start,
+      "last_index": start + len(results) - 1,
+      "results": results,
+    }
+    response = json_response(data)
+    response.charset = "utf-8"
+    response.last_modified = last_modified
+  return response
 
 
 @ROUTES.get("/ledger/{ledger_name}/text")
 async def ledger_text(request):
-  response = read_ledger(request.match_info['ledger_name'])
-  text = []
-  for seq_no, txn in response:
-    if len(text):
+  if not TRUST_ANCHOR.ready:
+    return not_ready()
+
+  response = web.StreamResponse()
+  response.content_type = "text/plain"
+  response.charset = "utf-8"
+  await response.prepare(request)
+
+  rows = await TRUST_ANCHOR.get_txn_range(request.match_info["ledger_name"])
+
+  first = True
+  for seq_no, added, row in rows:
+    text = []
+    if not first:
       text.append("")
+    first = False
+    row = json.loads(row)
+    txn = row["txn"]
+    data = txn["data"]
+    metadata = txn["metadata"]
 
     type_name = INDY_TXN_TYPES.get(txn['type'], txn['type'])
     text.append("[" + str(seq_no) + "]  TYPE: " + type_name)
 
-    if type_name == "NYM":
-      text.append("DEST: " + txn['dest'])
+    ident = metadata.get('from')
+    if ident != None:
+      text.append("FROM: " + ident)
 
-      role = txn.get('role')
+    if type_name == "NYM":
+      text.append("DEST: " + data['dest'])
+
+      role = data.get('role')
       if role != None:
         role_name = INDY_ROLE_TYPES.get(role, role)
         text.append("ROLE: " + role_name)
 
-      verkey = txn.get('verkey')
+      verkey = data.get('verkey')
       if verkey != None:
         text.append("VERKEY: " + verkey)
-
-    ident = txn.get('identifier')
-    if ident != None:
-      text.append("IDENT: " + ident)
 
     txnTime = txn.get('txnTime')
     if txnTime != None:
       ftime = datetime.fromtimestamp(txnTime).strftime('%Y-%m-%d %H:%M:%S')
       text.append("TIME: " + ftime)
 
-    reqId = txn.get('reqId')
+    reqId = metadata.get('reqId')
     if reqId != None:
       text.append("REQ ID: " + str(reqId))
 
-    refNo = txn.get('ref')
+    refNo = data.get('ref')
     if refNo != None:
       text.append("REF: " + str(refNo))
 
-    txnId = txn.get('txnId')
+    txnId = row['txnMetadata'].get('txnId')
     if txnId != None:
       text.append("TXN ID: " + txnId)
 
     if type_name == "SCHEMA" or type_name == "CLAIM_DEF" or type_name == "NODE":
-      data = txn.get('data')
+      data = data.get('data')
       text.append("DATA:")
       text.append(json.dumps(data, indent=4))
 
-    sig = txn.get('signature')
+    sig = data.get('signature')
     if sig != None:
       text.append("SIGNATURE: " + sig)
 
-    sig_type = txn.get('signature_type')
+    sig_type = data.get('signature_type')
     if sig_type != None:
       text.append("SIGNATURE TYPE: " + sig_type)
 
-  return web.Response(text="\n".join(text))
+    text.append("")
+    await response.write("\n".join(text).encode("utf-8"))
+
+  await response.write_eof()
+  return response
 
 
 @ROUTES.get("/ledger/{ledger_name}/{sequence_number:\d+}")
@@ -224,10 +204,12 @@ async def ledger_seq(request):
   seq_no = int(request.match_info['sequence_number'])
   ledger = request.match_info['ledger_name']
   try:
-    data = await TRUST_ANCHOR.get_txn(seq_no, ledger)
+    data = await TRUST_ANCHOR.get_txn(ledger, seq_no)
+    if not data:
+      return web.Response(status=404)
   except NotReadyException:
-    return web.Response(status=503)
-  return json_response(data)
+    return not_ready()
+  return json_response(json.loads(data[2]))
 
 
 # Expose genesis transaction for easy connection.
@@ -244,7 +226,7 @@ async def genesis(request):
 @ROUTES.post('/register')
 async def register(request):
   if not TRUST_ANCHOR.ready:
-    return web.Response(status=503)
+    return not_ready()
 
   body = await request.json()
   if not body:
@@ -277,11 +259,10 @@ async def register(request):
   if not did or not verkey:
     did, verkey = await TRUST_ANCHOR.seed_to_did(seed)
 
-  print('\n\nRegister agent\n\n')
   try:
     await TRUST_ANCHOR.register_did(did, verkey, alias, role)
   except NotReadyException:
-    return web.Response(status=503)
+    return not_ready()
 
   return json_response({
     'seed': seed,
@@ -291,13 +272,13 @@ async def register(request):
 
 
 async def boot(app):
-  print('Creating trust anchor...')
+  LOGGER.info('Creating trust anchor...')
   init = app['anchor_init'] = app.loop.create_task(TRUST_ANCHOR.open())
-  init.add_done_callback(lambda _task: print('--- Trust anchor initialized ---'))
+  init.add_done_callback(lambda _task: LOGGER.info('--- Trust anchor initialized ---'))
 
 
 if __name__ == '__main__':
   APP.add_routes(ROUTES)
   APP.on_startup.append(boot)
-  print('Running webserver...')
+  LOGGER.info('Running webserver...')
   web.run_app(APP, host='0.0.0.0', port=8000)
