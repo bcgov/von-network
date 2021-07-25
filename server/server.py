44eed13242c2f8415e67350c889aec9750c73dff
Aberasturi <gabriel.aberasturi@versia.com>
2021-07-25 13:02:33 +0200

    "Error loading transation data" after add new nodes.
    Signed-off-by: Aberasturi <gabriel.aberasturi@versia.com>

--- server/server.py
+++ server/server.py
@@ -154,7 +154,10 @@ async def ledger_json(request):
     last_modified = None
     results = []
     for row in rows:
-        last_modified = max(last_modified, row[1]) if last_modified else row[1]
+        try:
+            last_modified = max(last_modified, row[1]) if last_modified else row[1]
+        except TypeError:
+            last_modified = row[1]
         results.append(json.loads(row[3]))
     if not results and page > 1:
         data = {"detail": "Invalid page."}
