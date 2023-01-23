885bc67cfa32b59ee4f3ee52b7288c25cfbe91f1
alexander.shenshin <alexander.shenshin@dsr-corporation.com>
2023-01-23 19:26:56 +0300

    Add pip install to start_webserver.sh
    Signed-off-by: alexander.shenshin <alexander.shenshin@dsr-corporation.com>

--- scripts/start_webserver.sh
+++ scripts/start_webserver.sh
@@ -6,6 +6,9 @@ if [ ! -f "/home/indy/config/genesis.txn" ] && [ -z "${GENESIS_URL}" ] && [ -z "
   bash ./scripts/init_genesis.sh
 fi
 
+pip3 install -U pip &&
+pip install --no-cache-dir -r server/requirements.txt
+
 # link node ledgers where webserver can find them
 #for node in 1 2 3 4; do
 #    ln -sfn /home/indy/.mnt/node${node}/sandbox/data/Node${node} \
