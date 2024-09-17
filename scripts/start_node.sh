dba4685216b4328ac1ec8d8e5a51ccebb7411ad3
gramthanos <gramthanos@gmail.com>
2024-09-17 12:47:34 +0300

    added ability to change the default node ports

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -4,8 +4,16 @@ set -e
 
 HOST="${HOST:-0.0.0.0}"
 export NODE_NUM="${1}"
-START_PORT="9700"
-NODE_PORT=$((START_PORT + ( NODE_NUM * 2 ) - 1 ))
+DEFAULT_START_PORT="9700"
+NODE_PORT1="${2}"
+NODE_PORT2="${3}"
+
+if [[ -z "$NODE_PORT1" ]] ; then
+    NODE_PORT1=$(( DEFAULT_START_PORT + ( NODE_NUM * 2 ) - 1 ))
+fi
+if [[ -z "$NODE_PORT2" ]] ; then
+    NODE_PORT2=$(( NODE_PORT1 + 1 ))
+fi
 
 if [ ! -d "/home/indy/ledger/sandbox/keys" ]; then
     echo "Ledger does not exist - Creating..."
@@ -13,5 +21,5 @@ if [ ! -d "/home/indy/ledger/sandbox/keys" ]; then
 fi
 
 
-echo start_indy_node "Node""$NODE_NUM" $HOST $NODE_PORT $HOST $(( NODE_PORT + 1 ))
-start_indy_node "Node""$NODE_NUM" $HOST $NODE_PORT $HOST $(( NODE_PORT + 1 ))
+echo start_indy_node "Node""$NODE_NUM" $HOST $NODE_PORT1 $HOST $NODE_PORT2
+start_indy_node "Node""$NODE_NUM" $HOST $NODE_PORT1 $HOST $NODE_PORT2
